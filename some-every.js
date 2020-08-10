5347a6f2e5cc95e2315354e54953aa852faf91f6
Marina8989 <marina.mill209@gmail.com>
2020-08-09 23:23:07 -0700

    some every

--- some-every.js
+++ some-every.js
@@ -37,6 +37,7 @@ function hasAZero(num) {
 }
 console.log(hasAZero(3332123213101232321)) // true
 console.log(hasAZero(1212121)) // false
+
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
 
@@ -45,7 +46,16 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    const odd = arr.every(function (item) {
+        if (item % 2 == 1) {
+            return true;
+        }
+    });
+    return odd;
+}
+console.log(hasOnlyOddNumbers([1, 3, 5, 7])) // true
+console.log(hasOnlyOddNumbers([1, 2, 3, 5, 7])) // false
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -72,7 +82,23 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    const hasKey = arr.every(function (item) {
+        if (item.hasOwnProperty(key)) {
+            return true;
+        }
+    });
+    return hasKey;
+}
+var arr = [
+    { title: "Instructor", first: "Elie", last: "Schoppik" },
+    { title: "Instructor", first: "Tim", last: "Garcia", isCatOwner: true },
+    { title: "Instructor", first: "Matt", last: "Lane" },
+    { title: "Instructor", first: "Colt", last: "Steele", isCatOwner: true },
+];
+
+console.log(hasCertainKey(arr, 'first')) // true
+console.log(hasCertainKey(arr, 'isCatOwner')) // false
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -90,4 +116,21 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    const hasVal = arr.every(function (item) {
+        if (item[key] === searchValue) {
+            return true;
+        }
+    });
+    return hasVal;
+}
+
+var arr = [
+    { title: "Instructor", first: 'Elie', last: "Schoppik" },
+    { title: "Instructor", first: 'Tim', last: "Garcia", isCatOwner: true },
+    { title: "Instructor", first: 'Matt', last: "Lane" },
+    { title: "Instructor", first: 'Colt', last: "Steele", isCatOwner: true }
+]
+
+console.log(hasCertainValue(arr, 'title', 'Instructor')) // true
+console.log(hasCertainValue(arr, 'first', 'Elie')) // false
